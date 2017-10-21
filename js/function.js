5389bdf9b1c781cd54cb6f1da7587896fa122406
Bartek <bartekma89@gmail.com>
2017-10-21 19:53:55 +0200

    initial commit

---
+++ js/function.js
@@ -0,0 +1,17 @@
+var triangle1Area = getTriangleArea(10,6);
+var triangle2Area = getTriangleArea(12,7.2);
+var triangle3Area = getTriangleArea(10,-3);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
+
+function getTriangleArea(a, h) {
+	if (a <= 0 || h <= 0) {
+		alert('Nieprawidłowe dane');
+		console.log('Nieprawidłowe dane');
+		return;
+	}
+
+	return a * h / 2;
+}
