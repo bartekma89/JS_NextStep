ee4b493e51dc2500ae583bdc1b57a144272327cc
Bartek <bartekma89@gmail.com>
2017-10-22 20:09:15 +0200

    fixed bugs in target of christmas tree

--- js/loops.js
+++ js/loops.js
@@ -1,11 +1,11 @@
-drawTreeV1(7);
-drawTreeV2(7);
+drawTreeV1(5);
+drawTreeV2(5);
 
 function drawTreeV1(row) {
 
 	for (var i = 0, star; i < row; i++) {
 		star = '';
-		for (var j = 0; j < i; j++) {
+		for (var j = 0; j <= i; j++) {
 			star += '*';
 		}
 		console.log(star);
@@ -13,7 +13,7 @@ function drawTreeV1(row) {
 }
 
 function drawTreeV2(row) {
-	for (var i = 0, star = ''; i < row; i++, star += '*') {
+	for (var i = 0, star = ''; i <= row; i++, star += '*') {
 		console.log(star);
 	}
 }
